bbdff91a15fe486c0559f4ad43b590a3758c1c7a
frost <frostik1ful@gmail.com>
2020-10-11 21:24:50 +0300

    main gameplay js script moved to separate file, deleted some unnecessary files, some settings changed

--- src/main/java/com/durak/DurakartApplication.java
+++ src/main/java/com/durak/DurakartApplication.java
@@ -2,9 +2,10 @@ package com.durak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DurakartApplication {
+public class DurakartApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DurakartApplication.class, args);
